6773d857156aecf1b3cdb35f58deec87bb2e87e7
ramfort <rahulmohan@wazirx.com>
2021-06-30 10:59:26 +0530

    feat (routes) mount sidekiq on base url

--- config/routes.rb
+++ config/routes.rb
@@ -6,6 +6,6 @@ require 'sidekiq/prometheus/exporter'
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 
-  mount Sidekiq::Web => "/sidekiq"
+  mount Sidekiq::Web => "/"
   mount Sidekiq::Prometheus::Exporter => '/metrics'
 end
